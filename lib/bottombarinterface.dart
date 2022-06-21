219fc4033aabbf6cb27b2c3d35155718066051e4
Rob <a01285539@tec.mx>
2022-06-21 12:33:56 -0500

    Update (Android)
    - Name changed
    - Bugs fixed in compilation in Android

--- lib/bottombarinterface.dart
+++ lib/bottombarinterface.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 
 class BottomBarInterface extends StatefulWidget {
@@ -8,91 +9,67 @@ class BottomBarInterface extends StatefulWidget {
 }
 
 class _BottomBarInterfaceState extends State<BottomBarInterface> {
-  //Variables and methods
-  int selectedIndex = 0;
-  void onItemTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
+//
+  int _currentIndex = 0;
+  late PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
   }
 
-  static const List<Widget> _pages = <Widget>[
-    Icon(
-      Icons.call,
-      size: 150,
-    ),
-    Icon(
-      Icons.camera,
-      size: 150,
-    ),
-    Icon(
-      Icons.chat,
-      size: 150,
-    ),
-  ];
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
+      children: <Widget>[
         Scaffold(
-            body: Center(
-              child: _pages.elementAt(selectedIndex), //New
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              selectedItemColor: Colors.amberAccent,
-              backgroundColor: Colors.indigo,
-              selectedFontSize: 15,
-              selectedIconTheme: const IconThemeData(color: Colors.amberAccent, size: 32),
-              selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-              unselectedFontSize: 10,
-              showUnselectedLabels: false,
-              iconSize: 28,
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.call),
-                  label: "Calls",
+          body: SizedBox.expand(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() => _currentIndex = index);
+              },
+              children: <Widget>[
+                Container(
+                  color: Colors.grey,
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.camera),
-                  label: "Camera",
+                Container(
+                  color: Colors.white,
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.chat),
-                  label: "Chats",
+                Container(
+                  color: Colors.white,
+                ),
+                Container(
+                  color: Colors.white,
                 ),
               ],
-              currentIndex: selectedIndex,
-              onTap: onItemTapped,
-            )
-
-            /* SizedBox(
-              height: 100,
-              child: BottomAppBar(
-                color: Colors.indigo,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [],
-                ),
-              ),
-            ), */
-            
             ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 90),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 50,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-                color: Colors.indigo,
-              ),
-            ),
+          ),
+          bottomNavigationBar: BottomNavyBar(
+            selectedIndex: _currentIndex,
+            onItemSelected: (index) {
+              setState(() => _currentIndex = index);
+              _pageController.jumpToPage(index);
+            },
+            items: <BottomNavyBarItem>[
+              BottomNavyBarItem(
+                  title: const Text('Item One'), icon: const Icon(Icons.home)),
+              BottomNavyBarItem(
+                  title: const Text('Item Two'), icon: const Icon(Icons.apps)),
+              BottomNavyBarItem(
+                  title: const Text('Item Three'),
+                  icon: const Icon(Icons.chat_bubble)),
+              BottomNavyBarItem(
+                  title: const Text('Item Four'),
+                  icon: const Icon(Icons.settings)),
+            ],
           ),
         ),
       ],
