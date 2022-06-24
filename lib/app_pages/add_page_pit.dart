8ce498cce9cc533c12cdc25f7eb13e182673ab86
Dread5 <58833781+AndromedaHelix@users.noreply.github.com>
2022-06-24 10:50:09 -0500

    Finished design & functionality for AddPagePit

--- lib/app_pages/add_page_pit.dart
+++ lib/app_pages/add_page_pit.dart
@@ -33,13 +33,13 @@ class _AddPagePitState extends State<AddPagePit> {
       FormController formController = FormController((String response) {
         print(response);
         if (response == FormController.STATUS_SUCCESS) {
-          _showSnackBar("Se han enviado los datos");
+          _showSnackBar("Data has been sent");
         } else {
-          _showSnackBar("Un error ha ocurrido, vuelve a intentarlo");
+          _showSnackBar("An error has ocurred, try again later");
         }
       });
 
-      _showSnackBar("Enviando informacion");
+      _showSnackBar("Sending information");
       formController.submitForm(feedbackForm);
     }
   }
@@ -50,7 +50,7 @@ class _AddPagePitState extends State<AddPagePit> {
     );
 
     ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text("Datos enviados")));
+        .showSnackBar(SnackBar(content: Text("Data sent")));
 
     //_scaffoldKey.currentState.showSnackBar(snackbar);
   }
@@ -62,7 +62,7 @@ class _AddPagePitState extends State<AddPagePit> {
       body: Form(
         key: _formKey,
         child: Column(
-          children: <Widget>[
+          children: [
             Padding(
               padding: const EdgeInsets.only(top: 50.0, left: 10.0),
               child: Row(
@@ -93,56 +93,254 @@ class _AddPagePitState extends State<AddPagePit> {
               topPadding: 0,
             ),
             //LO DE ROB TODO: BORRAR
-            TextFormField(
-              controller: nameteamController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Enter Valid Name";
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(hintText: "Nombre del equipo"),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 0,
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 10.0, left: 30),
+                          child: Text(
+                            "Team name",
+                            style: TextStyle(
+                              fontFamily: "Oxygen",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 10.0, right: 30, left: 30),
+                        child: TextFormField(
+                          controller: nameteamController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter valid answer";
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Enter team name",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: const BorderSide(),
+                            ),
+
+                            //fillColor: Colors.green
+                          ),
+                          style: const TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                      ),
+
+                      // TextFormField(
+                      //   controller: nameteamController,
+                      //   validator: (value) {
+                      //     if (value!.isEmpty) {
+                      //       return "Enter Valid Name";
+                      //     } else {
+                      //       return null;
+                      //     }
+                      //   },
+                      //   decoration: InputDecoration(hintText: "Nombre del equipo"),
+                      // ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 10.0, left: 30),
+                          child: Text(
+                            "Team number",
+                            style: TextStyle(
+                              fontFamily: "Oxygen",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 10.0, right: 30, left: 30),
+                        child: TextFormField(
+                          controller: numteamController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter valid answer";
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Enter team number",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: const BorderSide(),
+                            ),
+
+                            //fillColor: Colors.green
+                          ),
+                          keyboardType: TextInputType.number,
+                          style: const TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                      ),
+
+                      // TextFormField(
+                      //   controller: numteamController,
+                      //   validator: (value) {
+                      //     if (value!.isEmpty) {
+                      //       return "Enter Valid Name";
+                      //     } else {
+                      //       return null;
+                      //     }
+                      //   },
+                      //   decoration: InputDecoration(hintText: "Numero de equipo"),
+                      // ),
+
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 10.0, left: 30),
+                          child: Text(
+                            "Chassis type",
+                            style: TextStyle(
+                              fontFamily: "Oxygen",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 10.0, right: 30, left: 30),
+                        child: TextFormField(
+                          controller: chasisController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter valid answer";
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Enter chassis type",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: const BorderSide(),
+                            ),
+
+                            //fillColor: Colors.green
+                          ),
+                          style: const TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                      ),
+
+                      // TextFormField(
+                      //   controller: chasisController,
+                      //   validator: (value) {
+                      //     if (value!.isEmpty) {
+                      //       return "Enter Valid Name";
+                      //     } else {
+                      //       return null;
+                      //     }
+                      //   },
+                      //   decoration: InputDecoration(hintText: "Tipo de chasis"),
+                      // ),
+
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 10.0, left: 30),
+                          child: Text(
+                            "Wheel type",
+                            style: TextStyle(
+                              fontFamily: "Oxygen",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 10.0, right: 30, left: 30),
+                        child: TextFormField(
+                          controller: wheelController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter valid answer";
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Enter wheel type",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: const BorderSide(),
+                            ),
+
+                            //fillColor: Colors.green
+                          ),
+                          style: const TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                      ),
+
+                      // TextFormField(
+                      //   controller: wheelController,
+                      //   validator: (value) {
+                      //     if (value!.isEmpty) {
+                      //       return "Enter Valid Name";
+                      //     } else {
+                      //       return null;
+                      //     }
+                      //   },
+                      //   decoration: InputDecoration(hintText: "Tipo de llantas"),
+                      // ),
+                      ElevatedButton(
+                        onPressed: () {
+                          _submitForm();
+                        },
+                        child: Text("Save"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-            TextFormField(
-              controller: numteamController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Enter Valid Name";
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(hintText: "Numero de equipo"),
-            ),
-            TextFormField(
-              controller: chasisController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Enter Valid Name";
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(hintText: "Tipo de chasis"),
-            ),
-            TextFormField(
-              controller: wheelController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Enter Valid Name";
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(hintText: "Tipo de llantas"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _submitForm();
-              },
-              child: Text("Guardar"),
-            )
           ],
         ),
       ),
