c2ca6d5d9674d13d764032221961d91d23883c20
Dread5 <58833781+AndromedaHelix@users.noreply.github.com>
2022-06-25 08:51:30 -0500

    Added background column

--- lib/app_pages/addpagemanager.dart
+++ lib/app_pages/addpagemanager.dart
@@ -12,74 +12,101 @@ class AddPageManager extends StatefulWidget {
 class _AddPageManagerState extends State<AddPageManager> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 200),
-      child: Column(
-        children: [
-          const Text(
-            "Choose a scouting sheet",
-            style: TextStyle(
-              fontSize: 20,
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 125.0),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Image(
+                    image: AssetImage(
+                        "assets/images/background/back1.jpeg")),
+                Image(
+                    image: AssetImage(
+                        "assets/images/background/back2.jpeg")),
+                Image(
+                    image: AssetImage(
+                        "assets/images/background/back1.jpeg"))
+              ],
             ),
           ),
-          const SizedBox(
-            height: 75,
-          ),
-          SizedBox(
-            height: 60,
-            width: 200,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const AddPageMatch()));
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.indigo),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 200),
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  "Choose a scouting sheet",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              child: const Text(
-                "Match scouting",
-                style: TextStyle(
-                  fontSize: 20,
+                const SizedBox(
+                  height: 75,
                 ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 80,
-          ),
-          SizedBox(
-            height: 60,
-            width: 200,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const AddPagePit()));
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.indigo),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                SizedBox(
+                  height: 60,
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const AddPageMatch()));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.indigo),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Match scouting",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              child: const Text(
-                "Pit scouting",
-                style: TextStyle(
-                  fontSize: 20,
+                const SizedBox(
+                  height: 80,
                 ),
-              ),
+                SizedBox(
+                  height: 60,
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const AddPagePit()));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.indigo),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Pit scouting",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
