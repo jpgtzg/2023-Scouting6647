c431b59ac9befc271a6aeebc37572175b1260827
Dread5 <58833781+AndromedaHelix@users.noreply.github.com>
2022-07-21 13:54:56 -0500

    Styled Pit System

--- lib/robot_gallery/pit/pitdetails.dart
+++ lib/robot_gallery/pit/pitdetails.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:scouting_app/newsystem/fetch/pit.dart';
 import 'package:scouting_app/newsystem/fetch/newpitcontroller.dart';
+import 'package:scouting_app/widgets/formdivider.dart';
 
 class PitDetails extends StatefulWidget {
   late NewPitController newPitController = new NewPitController();
   String teamNum;
-  PitDetails(this.teamNum);
+  String image;
+
+  PitDetails(this.teamNum, this.image);
 
   @override
   State<PitDetails> createState() => _PitDetailsState();
@@ -14,94 +17,138 @@ class PitDetails extends StatefulWidget {
 class _PitDetailsState extends State<PitDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder<Pit?>(
-        future: widget.newPitController.getById(int.parse(widget.teamNum)),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+    return Scaffold(
+      body: Container(
+        child: FutureBuilder<Pit?>(
+          future: widget.newPitController.getById(int.parse(widget.teamNum)),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-          final pit = snapshot.data;
+            final pit = snapshot.data;
 
-          if (pit != null) {
-            return Text(
-              "Number: " +
-                  (pit.number ?? '') +
-                  " Name: " +
-                  (pit.name ?? '') +
-                  " Chassis: " +
-                  (pit.chassis ?? '') +
-                  " Wheel Type: " +
-                  (pit.wheelType ?? ''),
-            );
-          } else {
-            return Text("No data");
-          }
-        },
+            if (pit != null) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: ScrollPhysics(),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xff4DAAE8).withOpacity(0.85),
+                          borderRadius: BorderRadius.all(Radius.circular(35.0)),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Container(
+                              height: 320,
+                              width: 320,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    widget.image,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 30.0, right: 30),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    "Team " + (pit.number ?? ''),
+                                    style: const TextStyle(
+                                      fontFamily: "Oxygen",
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black87,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text(
+                                    (pit.name ?? ''),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black87,
+                                        fontSize: 20,
+                                      ),
+                                    // ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 30.0, right: 30),
+                              child: const SizedBox(
+                                // width: 320,
+                                child: Divider(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    bottom: 10.0, left: 30),
+                                child: Text(
+                                  "Robot Info",
+                                  style: const TextStyle(
+                                    fontFamily: "Oxygen",
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "Chassis: " + (pit.chassis ?? ''),
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.black87),
+                            ),
+                            Text(
+                              "Wheel Type: " + (pit.wheelType ?? ''),
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.black87),
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Text("No data");
+            }
+          },
+        ),
       ),
     );
   }
 }
-
-
-/* FutureBuilder<List<Pit>>(
-        future: widget.newPitController.getAll(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final pit = snapshot.data;
-
-          return Padding(
-            padding: const EdgeInsets.only(right : 24.0, left : 24.0),
-            child: ListView.builder(
-              itemCount: pit == null ? 2 : pit.length ,
-              itemBuilder: (context, index) => pit == null
-                  ? Text("No data given") //Just null check
-                  : Container(
-                      padding: EdgeInsets.only(
-                        top: 20,
-                        bottom: 20,
-                      ),
-                      child: Text(
-                        "Number: " +
-                            (pit[index].number ?? '') +
-                            " Name: " +
-                            (pit[index].name ?? '') +
-                            " Chassis: " +
-                            (pit[index].chassis ?? '') +
-                            " Wheel Type: " +
-                            (pit[index].wheelType ?? ''),
-                      ),
-                    ),
-            ),
-          );
-        },
-      ), */
-
-/* FutureBuilder<Pit?>(
-        future: widget.newPitController.getById(6647),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final pit = snapshot.data;
-
-          if(pit != null){
-            return Text(pit.name.toString());
-          }
-
-          else{
-            return Text("A");
-          }
-        },
-      ), */
